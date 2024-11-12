640382c862e7b13ccc18e691d17763bf80293e5f
paupaudragon <u1432080@utah.edu>
2024-11-12 13:47:36 -0700

    Added Django models and connect to RDS, tested with json response

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,11 @@
+from . import views
 from django.urls import path
-from .views import simple_view
 
 urlpatterns = [
-    path('hello/', simple_view, name='simple_view')
+    # Example route for a simple view
+
+    # Routes for users, posts, and comments
+    path('users/', views.user_list, name='user_list'),
+    path('posts/', views.post_list, name='post_list'),
+    path('comments/', views.comment_list, name='comment_list'),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.http import JsonResponse
+from .models import User, Post, Comment
 
 
-def simple_view(request):
-    data = {
-        'message': 'Hello from Django!'
-    }
-    return JsonResponse(data)
+def user_list(request):
+    users = User.objects.values()  # Get all users as dictionaries
+    return JsonResponse(list(users), safe=False)
+
+
+def post_list(request):
+    posts = Post.objects.values()  # Get all posts as dictionaries
+    return JsonResponse(list(posts), safe=False)
+
+
+def comment_list(request):
+    comments = Comment.objects.values()  # Get all comments as dictionaries
+    return JsonResponse(list(comments), safe=False)
