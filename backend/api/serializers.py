6dcdd649de30fc000c4cf6f9cdb5131d96eca3d5
paupaudragon <u1432080@utah.edu>
2024-11-16 18:21:57 -0700

    browsing page is working

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -3,10 +3,16 @@ from .models import User, Post
 
 
 class UserSerializer(serializers.ModelSerializer):
+    profile_picture = serializers.SerializerMethodField()
+
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'bio',
                   'profile_picture', 'created_at', 'updated_at']
+
+    def get_profile_picture(self, obj):
+        # Assuming profile pictures are stored in the Flutter assets folder under "assets/images/"
+        return f'assets/user{obj.id}.png'
 
 
 class PostSerializer(serializers.ModelSerializer):
