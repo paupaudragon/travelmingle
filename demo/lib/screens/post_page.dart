02dff44cfda176501520a4cf4db99767bf4163bf
Tuan Nguyen Tran <andytran24519@GMAIL.COM>
2024-11-28 14:16:26 -0700

    Revert "Changes the color of reply and comment boxes"
    This reverts commit 828952f3ea57c172826aeed1817be73a4ebf4d00.

--- demo/lib/screens/post_page.dart
+++ demo/lib/screens/post_page.dart
@@ -518,7 +518,7 @@ class _PostPageState extends State<PostPage> {
   Widget buildCommentInput() {
     return Container(
       decoration: BoxDecoration(
-        color: const Color.fromARGB(255, 255, 249, 249), // Slightly darker background
+        color: const Color(0xFFF0F0F0), // Slightly darker background
         borderRadius: BorderRadius.circular(16), // Round the container
       ),
       padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
@@ -527,7 +527,7 @@ class _PostPageState extends State<PostPage> {
           if (activeReplyToCommentId != null)
             Container(
               decoration: BoxDecoration(
-                color: const Color.fromARGB(168, 214, 214, 214), // Darker reply box color
+                color: const Color(0xFFD6D6D6), // Darker reply box color
                 borderRadius: BorderRadius.circular(16),
               ),
               padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
@@ -562,7 +562,7 @@ class _PostPageState extends State<PostPage> {
                       borderSide: BorderSide.none,
                     ),
                     filled: true,
-                    fillColor: const Color.fromARGB(167, 232, 232, 232), // Light gray for input box
+                    fillColor: const Color(0xFFE8E8E8), // Light gray for input box
                   ),
                 ),
               ),
