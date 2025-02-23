bfc7e275ca8c2b4a4cd51417a2e471668a3966c9
paupaudragon <u1432080@utah.edu>
2025-02-23 16:20:09 -0700

    Add PostGIS and GDAL configuration for Elastic Beanstalk

---
+++ backend/.platform/hooks/postdeploy/01_setup_postgis.sh
@@ -0,0 +1,18 @@
+﻿#!/bin/bash
+
+# Enable debugging and exit on error
+set -xe
+
+# Install PostgreSQL and PostGIS
+amazon-linux-extras enable postgresql12
+yum clean metadata
+yum install -y postgresql postgresql-devel
+yum install -y gdal-devel
+yum install -y proj-devel
+yum install -y geos-devel
+yum install -y postgis31_12 postgis31_12-client
+
+# Set file permissions
+chmod 755 /usr/lib64/libgeos_c.so*
+chmod 755 /usr/lib64/libproj.so*
+chmod 755 /usr/lib64/libgdal.so*
