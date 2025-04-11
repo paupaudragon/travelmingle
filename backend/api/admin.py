af5bc39350826577da160d6441b0d0026f1d22b4
Ubuntu <ubuntu@ip-172-31-29-232.ec2.internal>
2025-04-11 22:22:58 +0000

    Add User model to admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Posts, PostImages
+from .models import Posts, PostImages,Users
 
 admin.site.register(Posts)
 admin.site.register(PostImages)
+admin.site.register(Users)
