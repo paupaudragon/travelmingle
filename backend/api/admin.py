52a285670e093bebc2aab70b6cf749b12fdb0eb6
paupaudragon <u1432080@utah.edu>
2025-04-21 20:00:14 -0600

    Add Comments model to the admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Posts, PostImages,Users
+from .models import Posts, PostImages,Users,Comments
 
 admin.site.register(Posts)
 admin.site.register(PostImages)
 admin.site.register(Users)
+admin.site.register(Comments)
